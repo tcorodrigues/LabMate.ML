d5059215245deb6a93f6f912558c84a551d846fa
Calum Hand <crh53@bath.ac.uk>
2021-07-13 11:11:14 +0100

    Setting initialiser to use command line arguments and take input boundaries through a yaml file. Overall saves users needing to alter any aspect of the source code when running. Also allows for dynamic update of headers etc since all sourced from yaml file.

--- initializer.py
+++ initializer.py
@@ -1,39 +1,32 @@
-import numpy as np
-import pandas as pd
-import itertools
+from itertools import product
+import argparse
 import os
 
-init_files_dir = r'./init_files'
+import yaml
+import pandas as pd
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--init_dir', type=str, action='store', default=r'./init_files',
+                    help='dir to save files to.')
+parser.add_argument('-b', '--boundary', type=str, action='store', default='Boundaries.yml',
+                    help='File containing boundary ranges.')
+parser.add_argument('-s', '--seed', type=int, action='store', default=1,
+                    help='Random seed for sampling.')
+parser.add_argument('-n', '--n_samples', type=int, action='store', default=10,
+                    help='Number conditions to sample.')
+args = parser.parse_args()
+
+
+init_files_dir = args.init_dir
 if not os.path.exists(init_files_dir):
     os.makedirs(init_files_dir)
 
-'''
-Section below creates lists for your reaction parameters. Change names of lists where appropriate
-'''
+with open(args.boundary, 'r') as f:
+    boundaries = yaml.load(f, yaml.SafeLoader)
 
-#For bigger lists use np.arange(min_value, max_value, step)
-Pyridine = [0.1, 0.2, 0.3] # in mmol
-Aldehyde = [0.1, 0.2, 0.3] # in mmol
-Isocyanide = [0.1, 0.2, 0.3] # in mmol
-Temperature = [10, 20, 40, 60, 80] # in C
-Solvent = [0.1, 0.25, 0.5, 1, 1.5] # in mL
-Catalyst = [0, 1, 2, 3, 4, 5, 7.5, 10] # in mol%
-Time = [5, 10, 15, 30, 60] # in minutes
+df = pd.DataFrame(data=product(*boundaries.values()), columns=boundaries.keys())
+df_random_data = df.sample(n=args.n_samples, random_state=args.seed)
 
-
-
-'''
-The following lines create all combos possible for the values listed above and saves as text file. Change names where appropriate.
-'''
-combinations = list(itertools.product(Pyridine, Aldehyde, Isocyanide, Temperature, Solvent, Catalyst, Time))
-df = pd.DataFrame(combinations)
-df.to_csv(os.path.join(init_files_dir,'all_combos.txt'), sep = '\t', header = ['Pyridine', 'Aldehyde', 'Isocyanide', 'Temperature', 'Solvent', 'Catalyst', 'Time'])
-
-
-'''
-Below, 10 random reaction are selected from all possible combinations. The reactions are stored in a text file. Change names of header as appropriate.
-'''
-
-random_data = df.sample(n=10, random_state=1)
-df_random_data = pd.DataFrame(random_data)
-df_random_data.to_csv(os.path.join(init_files_dir, 'train_data.txt'), sep= '\t', header = ['Pyridine', 'Aldehyde', 'Isocyanide', 'Temperature', 'Solvent', 'Catalyst', 'Time'])
+df.to_csv(os.path.join(init_files_dir, 'all_combos.txt'), sep='\t')
+df_random_data.to_csv(os.path.join(init_files_dir, 'train_data.txt'), sep='\t')
